ff046598a93cdaaa5e85b51ec663c8874c312a3d
mohamedabouseada <126615464+mohamed-abouseada121@users.noreply.github.com>
2025-12-05 21:49:34 +0200

    Add -y flag to docker installation command

--- install_docker.sh
+++ install_docker.sh
@@ -15,5 +15,5 @@ Signed-By: /etc/apt/keyrings/docker.asc \
 EOF
 
 sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 sudo docker run hello-world
