d20c03da35d0ba0aa243e5c76513b4e8f9dbd3a9
mohamedabouseada <126615464+mohamed-abouseada121@users.noreply.github.com>
2025-12-05 21:38:08 +0200

    Fix Docker GPG key path and install command

--- install_docker.sh
+++ install_docker.sh
@@ -1,8 +1,7 @@
 sudo apt update
 sudo apt install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyring
-s/docker.asc
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add the repository to Apt sources:
@@ -16,7 +15,5 @@ EOF
 
 sudo apt update
 
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin dock
-er-compose-plugin
-
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo docker run hello-world
