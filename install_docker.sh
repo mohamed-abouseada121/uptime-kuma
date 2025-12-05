948c7f2719d9195d768c538a765869e0a913f78a
mohamedabouseada <126615464+mohamed-abouseada121@users.noreply.github.com>
2025-12-05 22:03:52 +0200

    Refactor Docker installation script
    Updated the install script for Docker to improve reliability and added version checks.

--- install_docker.sh
+++ install_docker.sh
@@ -1,19 +1,25 @@
-# Add Docker's official GPG key:
-sudo apt update
-sudo apt install ca-certificates curl
+#!/bin/bash
+set -e
+
+sudo apt update -y
+sudo apt install -y ca-certificates curl gnupg
+
 sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
+sudo tee /etc/apt/keyrings/docker.asc > /dev/null
+
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# Add the repository to Apt sources:
-sudo tee /etc/apt/sources.list.d/docker.sources <<EOF \
-Types: deb  \
-URIs: https://download.docker.com/linux/ubuntu \
-Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") \
-Components: stable \
-Signed-By: /etc/apt/keyrings/docker.asc \
-EOF
+echo "Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc" | \
+sudo tee /etc/apt/sources.list.d/docker.sources > /dev/null
 
-sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-sudo docker run hello-world
+sudo apt update -y
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo docker --version
+sudo docker compose version
